95a49b35b6c5c7013f9cafdf8bb840ca9e95cdb5
makenasandra <makenawachira@gmail.com>
2025-06-17 11:53:07 +0300

    changes

--- client/src/components/EditableReviewCard.js
+++ client/src/components/EditableReviewCard.js
@@ -12,7 +12,7 @@ const Card = ({ id, content, onDelete }) => {
   const handleSaveClick = (newContent) => {
     setEditable(false);
     console.log(newContent)
-    // reviews[id-1].comment = 
+    reviews[id-1].comment = newContent;
     // Perform save operation (e.g., update content in the database)
   };
 
